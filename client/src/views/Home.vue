a9641fc9029e361d0c58c3d3e86b5a22ff17bfa2
Shir Zaguri <zagurishir@gmail.com>
2021-05-31 13:36:26 +0300

    fix(linter): now all linter config fixed and all files in the same format class style

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,14 +3,13 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default Vue.extend({
-  name: "Home",
-
+@Component({
   components: {
     HelloWorld,
   },
-});
+})
+export default class Home extends Vue {}
 </script>
