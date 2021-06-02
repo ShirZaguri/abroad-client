ed69dbedf7819d835a35488bcb010377fb90d372
shirza <zagurishir@gmail.com>
2021-06-02 18:54:07 +0300

    feat(TripPlan): Now you can see real trip attractions on time

--- client/src/components/TripPlan.vue
+++ client/src/components/TripPlan.vue
@@ -43,112 +43,6 @@ export default class TripPlan extends Vue {
 
     data() {
         return {
-            trip: [
-                {
-                    date: 'Thursday',
-                    places: [
-                        {
-                            name: 'Zaanse Schans',
-                            price: 15,
-                            type: 'zans',
-                            link: 'https://goo.gl/maps/AyXVKM5k6qVmDN3d6',
-                        },
-                    ],
-                },
-                {
-                    date: 'Friday',
-                    places: [
-                        {
-                            name: 'Hitorn',
-                            price: 18,
-                            type: 'trip',
-                        },
-                        // {
-                        //   name: "Adam's Lookout",
-                        //   price: 18,
-                        //   type: "adams",
-                        //   link: "https://goo.gl/maps/gPYBkAmLfSbTy3tg7",
-                        // },
-                        // {
-                        //   name: "Madame Tussauds",
-                        //   price: 15,
-                        //   type: "madam",
-                        //   link: "https://goo.gl/maps/Xtj7LzSLPbqEQirA9",
-                        // },
-                    ],
-                },
-                {
-                    date: 'Saturday',
-                    places: [
-                        {
-                            name: 'Anna Frank',
-                            price: 15,
-                            type: 'mus',
-                            link: 'https://goo.gl/maps/oxL2ahneEdrucKba6',
-                        },
-                        {
-                            name: 'Ripleys',
-                            price: 15,
-                            type: 'funnymus',
-                            link: 'https://goo.gl/maps/miJjNog1Vy2Fq5aZ7',
-                        },
-
-                        {
-                            name: 'Heineken',
-                            price: 15,
-                            type: 'beer',
-                            link:
-                                'https://g.page/heineken-experience-amsterdam?share',
-                        },
-                        {
-                            name: 'Holland Casino',
-                            price: 15,
-                            type: 'casino',
-                            link: 'https://goo.gl/maps/Z7osdfM8M4tKjMEWA',
-                        },
-                    ],
-                },
-                {
-                    date: 'Sunday',
-                    places: [
-                        {
-                            name: 'Efteling',
-                            price: 18,
-                            type: 'lunapark',
-                            link: 'https://goo.gl/maps/zZN36FmztzEMCefNA',
-                        },
-
-                        // {
-                        //   name: "Dangeoun",
-                        //   price: 15,
-                        //   type: "dang",
-                        //   link: "https://g.page/DungeonAMS?share",
-                        // },
-                    ],
-                },
-                {
-                    date: 'Monday',
-                    places: [
-                        { name: 'Bike Riding', price: 15, type: 'bike' },
-
-                        {
-                            name: 'Vondelpark',
-                            price: 15,
-                            type: 'nature',
-                            link: 'https://goo.gl/maps/HavjUwT7Pzv7rZZ76',
-                        },
-
-                        {
-                            name: 'Wnder',
-                            price: 15,
-                            type: 'jimbory',
-                            link: 'https://g.page/WONDR-Experience?share',
-                        },
-                        { name: 'Ice Bar', price: 15, type: 'icebar' },
-                    ],
-                },
-            ],
-
             dragOptions: {
                 animation: 200,
                 group: 'description',
@@ -163,29 +57,15 @@ export default class TripPlan extends Vue {
     }
 
     getDateDayName(date) {
-        const [dd, mm, yyyy] = date.split('-'),
-            convertedDate = new Date(yyyy, mm - 1, dd);
-        return convertedDate.toLocaleDateString('en-US', { weekday: 'long' });
+        return new Date(date).toLocaleDateString('en-US', { weekday: 'long' });
     }
 
     get attractionsByDate() {
         if (this.daysNames && this.attractions) {
-            console.log('In');
-            // return this.daysNames.map((day) => {
-            //     const dayPlaces = [];
-            //     dayPlaces.push(
-            //         this.attractions.filter(
-            //             (attraction) =>
-            //                 this.getDateDayName(attraction.date) === day
-            //         )
-            //     );
-            //     return dayPlaces;
-            // });
             return this.daysNames.map((day) => ({
                 date: day,
                 places: this.attractions.filter((attraction) => {
-                    console.log(this.getDateDayName(attraction.date));
-                    return this.getDateDayName(attraction.date) === day;
+                    return this.getDateDayName(attraction.details.date) === day;
                 }),
             }));
         } else {
@@ -206,16 +86,9 @@ export default class TripPlan extends Vue {
     }
 
     movePlace(place, newDate) {
-        const oldDay = this.$data.trip.find((day) =>
-            day.places.find(
-                (searchedPlace) => searchedPlace.name === place.name
-            )
-        );
-        const newDay = this.$data.trip.find((day) => day.date === newDate);
-        oldDay.places = oldDay.places.filter(
-            (searchedPlace) => searchedPlace.name !== place.name
-        );
-        newDay.places.push(place);
+        console.log(place);
+        this.attractions.find((attraction) => attraction.name === place.name);
+        // newDay.places.push(place);
     }
 }
 </script>
