0dd9f49b2e031cddcad9807bdc595268811418db
Shir Zaguri <zagurishir@gmail.com>
2021-06-24 15:30:58 +0300

    feat(map): now there is a base map component

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,19 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+
 import Vuesax from 'vuesax';
-import 'vuesax/dist/vuesax.css'; //Vuesax styles
+import 'vuesax/dist/vuesax.css';
+
+import { Map, TileLayer, OsmSource, Geoloc } from 'vuelayers';
+import 'vuelayers/lib/style.css';
 
 Vue.use(Vuesax, {});
+
+Vue.use(Map);
+Vue.use(TileLayer);
+Vue.use(OsmSource);
+Vue.use(Geoloc);
 
 Vue.config.productionTip = false;
 
