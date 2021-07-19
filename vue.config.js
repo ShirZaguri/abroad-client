bbe1254738fc3ab03ab813fc93925a3ba1ea2c11
Shir Zaguri <zagurishir@gmail.com>
2021-07-19 22:06:29 +0300

    fix(map-src-config): change map config so we can use src image from relative path

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,29 @@ module.exports = {
     devServer: {
         disableHostCheck: true,
     },
+    chainWebpack: (config) => {
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .tap((options) => {
+                options.transformAssetUrls = {
+                    'vl-style-icon': 'src',
+                    ...options.transformAssetUrls,
+                };
+                return options;
+            });
+    },
+    // chainWebpack: (config) => {
+    //     config.module
+    //         .rule('vue')
+    //         .use('vue-loader')
+    //         .loader('vue-loader')
+    //         .tap((options) => {
+    //             return {
+    //                 transformAssetUrls: {
+    //                     'vl-style-icon': 'src',
+    //                 },
+    //             };
+    //         });
+    // },
 };
