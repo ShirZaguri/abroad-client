9ff22cf28cc7a0c31b203d99b16dfc58fd9a3176
עמית בריקמן <ichsunnosaf@gmail.com>
2021-11-25 14:36:13 +0200

    fix(TopBar): Go back when icon clicked

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,10 +1,10 @@
 <template>
     <v-row
         justify="space-between"
-        class="countdown_bg ma-0 px-5 py-4"
+        class="countdown_bg ma-0 px-5 py-3"
         align="center"
     >
-        <span class="material-icons">arrow_back_ios_new</span>
+        <span class="material-icons" @click="goBack">arrow_back_ios_new</span>
     </v-row>
 </template>
 
@@ -12,11 +12,15 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({})
-export default class TopBar extends Vue {}
+export default class TopBar extends Vue {
+    goBack(): void {
+        this.$router.back();
+    }
+}
 </script>
 
 <style scoped>
 .countdown_bg {
-    background: linear-gradient(to right, #195bff, #6f42ff 40%);
+    background: linear-gradient(to right, #195bff, #6f42ff 80%);
 }
 </style>
