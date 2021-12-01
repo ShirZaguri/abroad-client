c42f52fe0db6d30d163c6140d5266f6152ea082a
Shir Zaguri <zagurishir@gmail.com>
2021-12-01 18:22:48 +0200

    wip: new add button for new attraction is added

--- src/services/attractionService.ts
+++ src/services/attractionService.ts
@@ -4,6 +4,7 @@ export default class AttractionService {
     static async updateAttraction(
         tripAttraction: tripAttractionType,
     ): Promise<tripAttractionType> {
+        console.log(JSON.stringify(tripAttraction));
         const data = await fetch(process.env.VUE_APP_UPDATE_ATTRACTION, {
             method: 'POST',
             headers: {
--- src/views/TripOverview.vue
+++ src/views/TripOverview.vue
@@ -1,12 +1,6 @@
 <template>
     <div id="main_holder">
         <div id="trip-info-holder">
-            <vs-button id="add-attraction-btn" gradient circle icon>
-                <v-icon size="15" color="white" class="pa-1">
-                    fas fa-plus
-                </v-icon>
-            </vs-button>
-
             <v-row
                 id="destination-bg"
                 class="ma-0 pa-5"
@@ -94,25 +88,5 @@ export default class TripMobile extends Vue {
 #destination-title {
     font-size: 13vw;
     line-height: 1.2;
-}
-
-#add-attraction-btn {
-    position: absolute;
-    height: 6.5vh;
-    width: 6.5vh;
-    bottom: 7vh;
-    right: 5vh;
-}
-
-#plus {
-    font-size: 3vh;
-}
-
-#attractions-holder {
-    height: 60vh;
-}
-
-.date-chip {
-    width: fit-content;
 }
 </style>
